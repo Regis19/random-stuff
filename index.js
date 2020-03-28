64a3419cc42dbe651cff0299bbe521f6ef8848a4
Danie Świerad <daniel.swierad@miten.pl>
2020-03-28 12:47:14 +0100

    Add interestingUrls, firstName, email and pets data

--- index.js
+++ index.js
@@ -4,12 +4,23 @@ const typeDefs = gql`
   type Query {
     greeting: String
     interestingUrls: [String]
+    firstName: String
+    email: String
+    pets: [String]
   }
 `;
 
 const data = {
   greeting: "Hello world",
-  interestingUrls: ["https://kursreacta.pl", "https://64bites.com"]
+  interestingUrls: [
+    "https://kursreacta.pl",
+    "https://64bites.com",
+    "https://www.hltv.org/",
+    "https://liquipedia.net/counterstrike/S-Tier_Tournaments"
+  ],
+  firstName: "John",
+  email: "john@example.com",
+  pets: ["Mittens", "Doggo", "Birb"]
 };
 
 const server = new ApolloServer({ typeDefs, rootValue: data });
